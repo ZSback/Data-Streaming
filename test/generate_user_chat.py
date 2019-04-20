78ee9dac61592b86c93f5ae2f6ebcfa3fd45ffb3
zhazha <42703555+zazha123@users.noreply.github.com>
2019-04-20 15:06:41 -0400

    test code done

--- test/generate_user_chat.py
+++ test/generate_user_chat.py
@@ -14,20 +14,41 @@ TEXT_LIST = ['暗示洗澡？',  '真实4.4', '这把 我上 我也行     我�
 two_bot_interval = 2
 bot_interval = 0.3
 
-def generate_realuser_chat():
-    print("USER IS RUNNING")
-    with open('danmutxt2.txt', 'a+') as fo:
-        while True:
+
+def generate_chat(fo):
+    print(f"Generate fake barrage info to {fo}")
+
+    with open(fo, 'a+') as fo:
+        # generate random number of real users
+
+        for _ in range(random.randint(1, 80)):
+
             timestamp = datetime.datetime.now()
             random_uid = uuid.uuid4()
             random_level = random.randint(1,50)
             random_text =  ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+
             result = f"{timestamp}{SPLIT_CHAR}{random_uid}{SPLIT_CHAR}{random_level}{SPLIT_CHAR}{random_text}\n"
 
-            time.sleep(0.3)
             fo.writelines(result)
+            time.sleep(0.05)
+
+        # generate a bot user
+        random_bot_uid = USER_LIST[random.randint(0, len(USER_LIST) - 1)]
+        bot_level = 1
+        for _ in range(3):
+            timestamp = datetime.datetime.now()
+            bot_text = TEXT_LIST[random.randint(0, len(TEXT_LIST) - 1)]
+
+            result = f"{timestamp}{SPLIT_CHAR}{random_bot_uid}{SPLIT_CHAR}{bot_level}{SPLIT_CHAR}{bot_text}\n"
+
+            fo.writelines(result)
+            time.sleep(0.05)
 
 
-generate_realuser_chat()
+# generate test file
+count = 0
+while True:
+    generate_chat(f'test{count}.txt')
 
-
+    count += 1
